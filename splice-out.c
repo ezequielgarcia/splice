5d418df6a5bb2189f68d5158171de0c80316426e
Jens Axboe <axboe@suse.de>
2006-04-20 14:58:16 +0200

    [PATCH] splice-out: Add migrate support (-m option)

--- splice-out.c
+++ splice-out.c
@@ -8,21 +8,46 @@
 
 #include "splice.h"
 
+static int splice_flags;
+
+static int usage(char *name)
+{
+	fprintf(stderr, "%s: [-m] out_file\n", name);
+	return 1;
+}
+
+static int parse_options(int argc, char *argv[])
+{
+	int c, index = 1;
+
+	while ((c = getopt(argc, argv, "m")) != -1) {
+		switch (c) {
+		case 'm':
+			splice_flags = SPLICE_F_MOVE;
+			index++;
+			break;
+		default:
+			return -1;
+		}
+	}
+
+	return index;
+}
+
 int main(int argc, char *argv[])
 {
-	int fd;
+	int fd, index;
 
-	if (argc < 2) {
-		printf("%s: outfile\n", argv[0]);
-		return 1;
-	}
+	index = parse_options(argc, argv);
+	if (index == -1 || index + 1 > argc)
+		return usage(argv[0]);
 
 	fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
 		return error("open");
 
 	do {
-		int ret = splice(STDIN_FILENO, NULL, fd, NULL, SPLICE_SIZE, 0);
+		int ret = splice(STDIN_FILENO, NULL, fd, NULL, SPLICE_SIZE, splice_flags);
 
 		if (ret < 0)
 			return error("splice");
